5ae5034473079e573bdd33f68819716077ef7f65
toihrk <toihrk@me.com>
2015-09-18 15:59:23 +0900

    init

---
+++ lib/itamae/plugin/recipe/nodebrew.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/nodebrew/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Nodebrew
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/nodebrew/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Nodebrew
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
